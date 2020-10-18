2f308b3503a076c45dbb8a69acc6863b9d9101fc
Masaya Kamei <km9smc6@gmail.com>
2020-10-18 16:44:27 +0900

    Add ft_strtrim

--- ft_strtrim_main.c
+++ ft_strtrim_main.c
@@ -9,9 +9,13 @@ int main(void)
 	printf("ft_strtrim(\" \\n\\nHello \\t\", \" \\n\\t\")	:%s\n", str1);
 
 	char *str2 = ft_strtrim(" \n \t", " \n\t");
-	printf("ft_strtrim(\" \\n \\t\")			:%s\n", str2);
+	printf("ft_strtrim(\" \\n \\t\", \" \\n\\t\")		:%s\n", str2);
+
+	char *str3 = ft_strtrim(" \na \t", " \n\t");
+	printf("ft_strtrim(\" \\na \\t\", \" \\n\\t\")		:%s\n", str3);
 
 	free(str1);
 	free(str2);
+	free(str3);
 	return (0);
 }
